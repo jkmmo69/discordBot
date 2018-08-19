df409122b7bcde8fe5869ca3be36538a8d091abe
jkmmo69 <36444374+jkmmo69@users.noreply.github.com>
2018-08-19 03:07:44 -0400

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -5,7 +5,7 @@ let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
 
-  //!warn @daeshan <reason>
+  //
   if(!message.member.hasPermission("KICK_MEMBERS")) return;
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if(!wUser) return message.reply("User is a nil value.");
